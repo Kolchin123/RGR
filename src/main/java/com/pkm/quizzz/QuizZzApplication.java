214d32bb89a46037f1e6583e1f8eed27f0b106ef
Kolchin123 <kolchin.0406@mail.ru>
2021-06-04 18:40:24 +0400

    popular quiz sorting

--- src/main/java/com/pkm/quizzz/QuizZzApplication.java
+++ src/main/java/com/pkm/quizzz/QuizZzApplication.java
@@ -12,7 +12,6 @@ public class QuizZzApplication {
 
 	//поиск
 	//админ доделать
-	//самый популярный
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuizZzApplication.class, args);
